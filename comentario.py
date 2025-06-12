ed668179e2be0aba8fc56a66bc7304a7d0c8a90d
nayeli <155994804+nayeli-guzman@users.noreply.github.com>
2025-06-11 23:30:09 -0500

    Add files via upload

--- comentario.py
+++ comentario.py
@@ -1,6 +1,7 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -8,6 +9,8 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    bucket_name = os.environ["BUCKET_NAME"] 
+
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -20,6 +23,20 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+
+    comentario_json = json.dumps(comentario)
+    archivo_nombre = f"comentarios/{tenant_id}/{uuidv1}.json"
+    s3 = boto3.client('s3')
+        
+    s3.put_object(
+            Bucket=bucket_name,
+            Key=archivo_nombre,
+            Body=comentario_json,  
+            ContentType='application/json'  
+        )
+        
+
+
     # Salida (json)
     print(comentario)
     return {
